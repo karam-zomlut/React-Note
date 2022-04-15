b12bf1125177309ece349120661fbc67a09a2187
karam-zomlut <zomlutk@gmail.com>
2022-04-15 22:51:14 +0300

    Add Setting Menu List

--- src/Components/SingleNote/index.jsx
+++ src/Components/SingleNote/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SettingMenu from '../SettingMenu';
 import './style.css';
 
 const SingleNote = () => {
@@ -6,12 +7,13 @@ const SingleNote = () => {
     <div className="single-note">
       <div className="details">
         <h2 className="title">Title here</h2>
-        <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. excepturi. Nobis esse magni delectus qui corrupti quam libero suscipit? Inventore, voluptates modi!</p>
+        <p className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum possimus harum nulla cumque, asperiores dignissimos facilis eius non, exercitationem ipsam culpa perferendis molestiae quaerat quam placeat recusandae molestias voluptate quisquam.</p>
       </div>
       <div className="bottom-content">
         <span className="date">April 3, 2022</span>
         <div className="actions">
           <button className="btn more"><i className='ri-more-fill'></i></button>
+          <SettingMenu />
         </div>
       </div>
     </div>
